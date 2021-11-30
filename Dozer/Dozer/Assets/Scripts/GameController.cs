2c428a80aa026bc57a155402d3c47b35f18b623a
ahmet <abayir41@gmail.com>
2021-11-30 22:31:50 +0300

    Camera Follower Added

--- Dozer/Dozer/Assets/Scripts/GameController.cs
+++ Dozer/Dozer/Assets/Scripts/GameController.cs
@@ -3,24 +3,28 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public float power;
+    [SerializeField] private GameObject dozerGameObject;
+    private Vector3 _cameraFarFromDozer;
+    private Transform _dozerTrans;
+    private Transform _camera;
     private ISteerSystem _steerSystem;
     private void Awake()
     {
-
+        //Paint the all buildings, cars, trees...
         var paintableObjs = FindObjectsOfType<ColorChanger>().OfType<IColorChangerRandomly>().ToList();
         foreach (var paintableObj in paintableObjs)
         {
             paintableObj.SelectColorRandomly();
         }
 
+        if (!(Camera.main is null)) _camera = Camera.main.transform;
+        _dozerTrans = dozerGameObject.transform;
         _steerSystem = GetComponent<BasicSteerSystem>();
-
-        
+        _cameraFarFromDozer = _camera.position - _dozerTrans.position;
     }
 
     private void Update()
     {
-        Debug.Log(_steerSystem.Angle);
+        _camera.position = _cameraFarFromDozer + _dozerTrans.position;
     }
 }
