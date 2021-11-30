db59155504b20f502ecf2f5a71e0b8d3a16468e1
ahmet <abayir41@gmail.com>
2021-11-30 22:26:34 +0300

    CarControllerVelocity

---
+++ Dozer/Dozer/Assets/Scripts/CarController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float velocityMultiplier;
+    private Rigidbody _rigidbody;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.inertiaTensorRotation = Quaternion.identity;
+        
+        
+    }
+
+    void Start()
+    {
+        _rigidbody.velocity = transform.forward * velocityMultiplier;
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = transform.forward * velocityMultiplier;
+    }
+}
