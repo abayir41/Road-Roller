c5e241c271d117f98392e64ae28a7e318eb00a2e
ahmet <abayir41@gmail.com>
2021-11-30 23:38:23 +0300

    Utilities

---
+++ Dozer/Dozer/Assets/Scripts/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utilities
+{
+    public static int PosOrNeg(float x)
+    {
+        if (x > 0)
+        {
+            return 1;
+        }
+        else if (x < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
