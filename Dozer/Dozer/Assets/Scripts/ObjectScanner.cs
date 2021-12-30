47bcb7d76ff37f3962e57c281e454e9eb37c9f33
ahmet <abayir41@gmail.com>
2021-12-30 23:59:20 +0300

    ObjectScanner

---
+++ Dozer/Dozer/Assets/Scripts/ObjectScanner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+interface IObjectScanner
+{
+    List<GameObject> ScannedObjects { get; }
+}
+public class ObjectScanner : MonoBehaviour, IObjectScanner
+{
+    public List<GameObject> ScannedObjects
+    {
+        get { return scannedObjects; }
+    }
+
+    [SerializeField]
+    private List<GameObject> scannedObjects;
+
+    private void Awake()
+    {
+        scannedObjects = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        scannedObjects.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    { 
+        scannedObjects.Remove(other.gameObject);
+    }
+}
