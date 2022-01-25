8515f1c329efc0e5a50c0961c44866308d527855
ahmet <abayir41@gmail.com>
2022-01-26 00:06:44 +0300

    Abstract Class Of LeaderBoardSystem

---
+++ Dozer/Dozer/Assets/Scripts/LeaderboardsAbstract.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class LeaderboardsAbstract : MonoBehaviour
+{
+    protected Dictionary<string, int> PlayerAndScoreDictionary;
+    protected abstract Action<string, int> ScoreChanged { get; set;} 
+    protected virtual void Awake()
+    {
+        PlayerAndScoreDictionary = new Dictionary<string, int>();
+    }
+
+    private void OnEnable()
+    {
+        ScoreChanged += AddScore;
+    }
+
+    private void OnDisable()
+    {
+        ScoreChanged -= AddScore;
+    }
+
+    public void SetScore(string player, int score)
+    {
+        if (PlayerAndScoreDictionary.ContainsKey(player))
+        {
+            PlayerAndScoreDictionary[player] = score;
+        }
+        else
+        {
+            PlayerAndScoreDictionary.Add(player,score);
+        }
+    }
+    public void AddScore(string player, int score)
+    {
+        if (PlayerAndScoreDictionary.ContainsKey(player))
+        {
+            PlayerAndScoreDictionary[player] += score;
+        }
+        else
+        {
+            PlayerAndScoreDictionary.Add(player,score);
+        }
+    }
+
+    public int PlayerCount => PlayerAndScoreDictionary.Count;
+
+    public abstract List<string> GetLeaderBoard(int playerCount);
+}
+
