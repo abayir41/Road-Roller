e941a7482a56c2873fa5991548041b593d3588bc
ahmet <abayir41@gmail.com>
2022-01-30 00:34:54 +0300

    Register System

---
+++ Dozer/Dozer/Assets/Scripts/RegisterSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+internal interface IRegisterSystem
+{
+    void SaveData(string key, int value);
+    void SaveData(string key, float value);
+    void SaveData(string key, string value);
+
+    int GetDataAsInt(string key);
+    float GetDataAsFloat(string key);
+    string GetDataAsString(string key);
+}
+public class RegisterSystem : MonoBehaviour , IRegisterSystem
+{
+    public void SaveData(string key, int value)
+    {
+        PlayerPrefs.SetInt(key,value);
+    }
+
+    public void SaveData(string key, float value)
+    {
+        PlayerPrefs.SetFloat(key,value);
+    }
+
+    public void SaveData(string key, string value)
+    {
+        PlayerPrefs.SetString(key,value);
+    }
+
+    public int GetDataAsInt(string key)
+    {
+        return PlayerPrefs.GetInt(key);
+    }
+
+    public float GetDataAsFloat(string key)
+    {
+        return PlayerPrefs.GetFloat(key);
+    }
+
+    public string GetDataAsString(string key)
+    {
+        return PlayerPrefs.GetString(key);
+    }
+}
