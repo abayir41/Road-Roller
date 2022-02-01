fab05d507c9be0a9ab85e3d074c28f5ca1bc3f33
ahmet <abayir41@gmail.com>
2022-02-01 20:26:13 +0300

    Skin Scriptable Object Created

---
+++ Dozer/Dozer/Assets/Scripts/SkinScriptable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Skin", menuName = "SkinMenu")]
+public class SkinScriptable : ScriptableObject , IPurchasableDozer
+{
+    public string ItemID => itemID;
+    [SerializeField] private string itemID;
+
+    public int Price => price;
+    [SerializeField] public int price;
+
+    public GameObject DozerSkin => dozerSkin;
+    [SerializeField] private GameObject dozerSkin;
+
+    public Image Preview => preview;
+    [SerializeField] private Image preview;
+
+}
