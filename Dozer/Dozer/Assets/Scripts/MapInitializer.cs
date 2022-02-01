b15eb7aa189c35cadec81c9c2c809c2b2695b373
ahmet <abayir41@gmail.com>
2022-02-01 23:08:20 +0300

    Map Initilizer

---
+++ Dozer/Dozer/Assets/Scripts/MapInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MapInitializer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> maps;
+    private void Start()
+    {
+        var ranInt = Random.Range(0, maps.Count);
+        Instantiate(maps[ranInt]);
+        Destroy(this);
+    }
+}
