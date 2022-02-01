0fd1497b037dbdbb78ef05ffd02a9d9a15d5b6c2
ahmet <abayir41@gmail.com>
2022-02-01 23:09:56 +0300

    Mini changes and Mesh renderer option

--- Dozer/Dozer/Assets/Scripts/SkinSystem.cs
+++ Dozer/Dozer/Assets/Scripts/SkinSystem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class SkinSystem : MonoBehaviour
 {
@@ -11,17 +11,27 @@ public class SkinSystem : MonoBehaviour
     {
         var visualPoint = GetComponent<CarSystem>().VisualPoint;
         var isAI = GetComponent<PlayerController>().IsAI;
+        var interactableObj = GetComponent<InteractableObj>();
+        GameObject skinGameObject;
         if (isAI)
         {
-            var random = new Random();
-            var randomSkin = GameController.Instance.AllSkins[random.Next(GameController.Instance.AllSkins.Count)];
-            var skin = Instantiate(randomSkin.DozerSkin, visualPoint);
+            var ranInt = Random.Range(0, GameController.Instance.AllSkins.Count);
+            var randomSkin = GameController.Instance.AllSkins[ranInt];
+            skinGameObject = Instantiate(randomSkin.DozerSkin, visualPoint);
         }
         else
         {
             var skinName = GameController.Instance.RegisterSystem.GetDataAsString(MarketSystem.SelectedSkin);
             var skinScriptable = GameController.Instance.AllSkins.First(skin => skin.ItemID == skinName);
-            var skinGO = Instantiate(skinScriptable.DozerSkin, visualPoint);
+            skinGameObject = Instantiate(skinScriptable.DozerSkin, visualPoint);
         }
+
+        var meshRenderers = skinGameObject.GetComponentsInChildren<MeshRenderer>();
+        foreach (var meshRenderer in meshRenderers)
+        {
+            interactableObj.meshRenderers.Add(meshRenderer);
+        }
+
+        this.enabled = false;
     }
 }
