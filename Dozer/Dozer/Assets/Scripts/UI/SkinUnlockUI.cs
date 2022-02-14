3192e55c35cb3ac48c94fe86df6acfa414de1f7e
ahmet <abayir41@gmail.com>
2022-02-15 02:38:34 +0300

    Skin Unlock System

---
+++ Dozer/Dozer/Assets/Scripts/UI/SkinUnlockUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinUnlockUI : MonoBehaviour
+{
+    [SerializeField] private RectMask2D mask;
+    [SerializeField] private RectTransform parentRectTransform;
+    private float _paddingAmount;
+    private float _parentHeight;
+
+    private void Awake()
+    {
+        _parentHeight = parentRectTransform.rect.height;
+    }
+
+    private void Start()
+    {
+        DOTween.Init();
+    }
+
+    public void ScrollTheImage(float percentage, float duration, Action callback = null)
+    {
+        DOTween.To(() => mask.padding.z, 
+            value => mask.padding.Set(0,0,0,value),
+            percentage/100*_parentHeight, duration)
+            .OnKill((() => callback?.Invoke()));
+    }
+}
