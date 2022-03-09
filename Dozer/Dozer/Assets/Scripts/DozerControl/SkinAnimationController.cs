c9d73a913d4acf272084b94b516c2e56f4afd725
ahmet <abayir41@gmail.com>
2022-03-09 16:08:59 +0300

    Tire Rolling

---
+++ Dozer/Dozer/Assets/Scripts/DozerControl/SkinAnimationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SkinAnimationController : MonoBehaviour
+{
+    
+    private Animator _animator;
+    
+    [Header("Config")] 
+    [SerializeField] private float angularVelocityMultiplier;
+
+    private static readonly int TireSpeedMultiplier = Animator.StringToHash("TireSpeedMultiplier");
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (GameController.Status != GameStatus.Playing)
+        {
+            UpdateTheTireAnimationSpeedMultiplier(0.0f);
+            return;
+        }
+        
+        UpdateTheTireAnimationSpeedMultiplier(angularVelocityMultiplier);
+    }
+
+    private void UpdateTheTireAnimationSpeedMultiplier(float multiplier)
+    {
+        _animator.SetFloat(TireSpeedMultiplier, multiplier);
+    }
+}
