cf8bf22623f97dee7441d9b73d1d4b032f329343
ahmet <abayir41@gmail.com>
2022-03-12 15:40:43 +0300

    MMVibration Imp

---
+++ Dozer/Dozer/Assets/asd.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class asd : MonoBehaviour
+{
+    public int avgFrameRate;
+    public TextMeshProUGUI display_Text;
+    public int avgFrameRate2;
+    public TextMeshProUGUI display_Text2;
+
+    private void Start()
+    {
+        StartCoroutine(CheckFPS());
+        StartCoroutine(CheckFPSFaster());
+    }
+
+    IEnumerator CheckFPS()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(CheckFPS());
+    }
+    IEnumerator CheckFPSFaster()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate2 = (int)current;
+        display_Text2.text = avgFrameRate2.ToString() + " FPS";
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(CheckFPSFaster());
+    }
+}
